795dbea0cdcc371a5037abe49e504cd8f1949c8e
tugrulguner <tugrulgunr@gmail.com>
2021-12-15 13:54:42 -0500

    test_api finalized

--- starter/test_api.py
+++ starter/test_api.py
@@ -37,3 +37,26 @@ def test_inference_1():
   uploading_data = json.dumps(uploading_data).encode('utf8')
   data_response = requests.post('http://127.0.0.1:8000/inference', uploading_data)
   assert data_response.json() != '<50k$' or data_response.json() != '>50k$'
+
+@test_app.post('/inference_check_2')
+def test_inference_2():
+  uploading_data = {
+    "age": 35,
+    "workclass": 'Private',
+    "fnlgt": 45780,
+    "education": 'Bachelors',
+    "education-num": 13,
+    "marital-status": 'Never-married',
+    "occupation": 'Prof-specialty',
+    "relationship": 'Not-in-family',
+    "race": 'Black',
+    "sex": 'Female',
+    "capital-gain": 3000,
+    "capital-loss": 500,
+    "hours-per-week": 40,
+    "native-country": 'Jamaica'
+  }
+
+  uploading_data = json.dumps(uploading_data).encode('utf8')
+  data_response = requests.post('http://127.0.0.1:8000/inference', uploading_data)
+  assert data_response.json()
