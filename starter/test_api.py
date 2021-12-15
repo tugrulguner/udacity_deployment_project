fc0b8eba4237f04e9a60b610fd3c40b3c612139c
tugrulguner <tugrulgunr@gmail.com>
2021-12-15 12:39:59 -0500

    test_api updated

--- starter/test_api.py
+++ starter/test_api.py
@@ -1,10 +1,16 @@
 import pytest
 from fastapi import FastAPI
 import requests
+import json
 
 test_app = FastAPI()
 
 @test_app.get('/')
+def test_root():
+  read_get = requests.get('http://127.0.0.1:8000/')
+  assert read_get.status_code == 200
+
+@test_app.get('/')
 def test_welcome():
-  read_get = requests.get('localhost:/8000/')
-  assert read_get.status_code == '200'
+  read_get_message = requests.get('http://127.0.0.1:8000/')
+  assert len(read_get_message.json()[0]) != 0
