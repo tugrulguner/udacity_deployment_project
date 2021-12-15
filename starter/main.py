4346f6bf53496af675790105a4491a3a2e79d16e
tugrulguner <tugrulgunr@gmail.com>
2021-12-15 15:57:27 -0500

    dvc specified for Heroku

--- starter/main.py
+++ starter/main.py
@@ -1,3 +1,5 @@
+
+import os
 from fastapi import FastAPI
 import joblib
 from pydantic import BaseModel
@@ -5,6 +7,12 @@ import pandas as pd
 
 from starter.ml.data import process_data
 from starter.ml.model import inference
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
 
