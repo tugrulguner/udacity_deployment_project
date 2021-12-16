384b863204476a7bc7dbc7148d581c7e3072ccce
tugrulguner <tugrulgunr@gmail.com>
2021-12-15 20:31:28 -0500

    pytest improved

--- starter/starter/ml/test_model.py
+++ starter/starter/ml/test_model.py
@@ -1,6 +1,7 @@
 import pytest
 import pandas as pd
 import joblib
+import os
 from sklearn.model_selection import train_test_split
 from ml.data import process_data
 from ml.model import train_model, compute_model_metrics, inference
@@ -9,7 +10,7 @@ from ml.model import train_model, compute_model_metrics, inference
 @pytest.fixture(scope='session')
 def input_data():
 
-    data = pd.read_csv('../../data/cleaned_data.csv')
+    data = pd.read_csv(os.get_cwd()+'../../data/cleaned_data.csv')
 
     train, test = train_test_split(data, test_size=0.20)
 
@@ -59,7 +60,7 @@ def test_compute_model_metrics(input_data):
 
 def test_inference(input_data):
 
-    model = joblib.load('../../model/RF_Classifier.pkl')
+    model = joblib.load(os.get_cwd()+'../../model/RF_Classifier.pkl')
 
     inference_test = inference(model, input_data[2])
 
